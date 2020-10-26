7a52574e4f2da97bd513d8d44449086b0f0f28fa
Daniel Marchand <t-dmarchand@microsoft.com>
2020-10-26 08:12:16 -0400

    fixed validate version number

--- utils/validate_version_consistency.py
+++ utils/validate_version_consistency.py
@@ -8,6 +8,7 @@ import json
 import os
 import re
 import sys
+from setuptools.config import read_configuration
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(SCRIPT_DIR, os.path.pardir)
@@ -18,9 +19,9 @@ with open(os.path.join(ROOT_DIR, 'aiida_testing/__init__.py')) as f:
     VERSION_INIT = re.search(MATCH_EXPR, f.read()).group(2).strip()  # type: ignore
 
 # Get the setup.json version number
-with open(os.path.join(ROOT_DIR, 'setup.json')) as f:
-    VERSION_JSON = json.load(f)["version"]
+SETUP_PATH = os.path.join(ROOT_DIR, 'setup.cfg')
+VERSION_CFG = read_configuration(SETUP_PATH)['metadata']['version']
 
-if VERSION_INIT != VERSION_JSON:
-    print("Version numbers don't match: init:'{}', json:'{}' ".format(VERSION_INIT, VERSION_JSON))
+if VERSION_INIT != VERSION_CFG:
+    print("Version numbers don't match: init:'{}', cfg:'{}' ".format(VERSION_INIT, VERSION_CFG))
     sys.exit(1)
